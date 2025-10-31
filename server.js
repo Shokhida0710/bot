a7936f2b060b2a189baeee09b6078da37ebab87e
shokhida <shohidaomonova008@gmail.com>
2025-10-31 18:01:58 +0500

    button +

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
-import TelegramBot from "node-telegram-bot-api"
-const TOKEN = '8204814513:AAFJU9_DXKFU7MklpYMd2_eS3NYaIUaz0k4'
-const bot = new TelegramBot(TOKEN, { polling: true })
 
-bot.on(`message`, function (msg) {
-    const chatId = msg.chat.id;
-    console.log(msg);
-    bot.sendMessage(chatId, "Xush kelibsiz")
+
+import TelegramBot from "node-telegram-bot-api";
+const TOKEN = '8204814513:AAFJU9_DXKFU7MklpYMd2_eS3NYaIUaz0k4'
+
+const bot = new TelegramBot(TOKEN, { polling: true });
+bot.on("message", function (msg) {
+
+  const chatId = msg.chat.id;
+  const firstName = msg.chat.first_name;
+  const text = msg.text;
+
+ 
+  console.log(msg);
+  bot.sendMessage(chatId, `Xush kelipsiz!, ${firstName} 👋`, {
+    reply_markup: {
+      keyboard: [
+        [{ text: "Boshlash 🔥"}],
+        [{ text: "Menu 🍔"}, { text: "Sozlamalar ⚙️"}]
+      ],
+      resize_keyboard:true
+    }
+  })
+
 });
 
-console.log("Bot is starting...");
-
+console.log("Bot ishga tushdi..");
 
